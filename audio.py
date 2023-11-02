106e7f02632f8d208e6d75a7ced66bbeb13568d5
Sanvi-Sundarrajan <93093295+Sanvi-Sundarrajan@users.noreply.github.com>
2023-11-02 22:01:59 +0530

    Update audio.py

--- audio.py
+++ audio.py
@@ -1,1 +1,18 @@
+!pip install transformers
+import soundfile as sf
+import numpy as np
+from transformers import Wav2Vec2Processor, Wav2Vec2Model
+
+processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base")
+model = Wav2Vec2Model.from_pretrained("facebook/wav2vec2-base")
+
+audio_data, sample_rate = sf.read("unseen_audio.wav")
+
+audio_array = np.array(audio_data)
+
+audio_features = processor(audio_array, sampling_rate=sample_rate)
+
+predictions = model(audio_features)
+
+print(predictions.logits.argmax())
 
