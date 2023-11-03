70a076382a724aa99c0b99b10c3ca034fb1829a6
Sanvi-Sundarrajan <93093295+Sanvi-Sundarrajan@users.noreply.github.com>
2023-11-04 03:53:32 +0530

    Update audio.py

--- audio.py
+++ audio.py
@@ -1,42 +1,28 @@
 !pip install transformers
-!pip install soundfile
-!pip install librosa
 !pip install torch
+!pip install soundfile
+!pip install wavio
+!pip install scripy
 
-import soundfile as sf
-import numpy as np
-import librosa
-from transformers import Wav2Vec2Processor, Wav2Vec2Model
+from transformers import Wav2Vec2Processor, Wav2Vec2ForMaskedLM
 import torch
-import warnings
-warnings.filterwarnings("ignore")
+import wavio
+import soundfile as sf
 
-processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base")
-model = Wav2Vec2Model.from_pretrained("facebook/wav2vec2-base")
+processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
 
-audio_data, sample_rate = sf.read("harvard.wav")
+model = Wav2Vec2ForMaskedLM.from_pretrained("facebook/wav2vec2-base-960h")
+audio, sample_rate = sf.read("ENG_M.wav")
 
-audio_array = np.array(audio_data)
+def predict_tone(audio_file):
 
-# Resample
-resampled_audio = librosa.resample(audio_array, orig_sr=sample_rate, target_sr=16000)
+  inputs = processor(audio, sampling_rate=sample_rate, return_tensors="pt", padding=True)
+  outputs = model(**inputs)
+  predicted_tone_id = outputs.logits.argmax(-1).item()
 
-# Extract features
-audio_features = processor(resampled_audio,
-                           sampling_rate=16000,
-                           return_tensors="pt").input_values
+  labels = ["neutral", "happy", "sad"]
+  predicted_tone = labels[predicted_tone_id]
+  return predicted_tone
 
-# Pad if needed
-min_size = 100
-if audio_features.shape[1] < min_size:
-  pad_amt = min_size - audio_features.shape[1]
-  audio_features = torch.nn.functional.pad(audio_features, (0, pad_amt))
-
-# Truncate
-max_len = 4000
-if audio_features.shape[1] > max_len:
-  audio_features = audio_features[:,:max_len]
-
-predictions = model(audio_features)
-
-print(predictions.logits.argmax())
+print(predict_tone)
+# Currently the emotion tags are'nt mapped with the predicted_tone_id , so it returns the id 
